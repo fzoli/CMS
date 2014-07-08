63bd587911895459a0d9639fe0ba1bede525356b
zoli <f.zoli@mailbox.hu>
2014-07-08 22:27:27 +0200

    TODO: deploy war

---
+++ CMS-web/src/main/java/Beans.java
@@ -0,0 +1,26 @@
+import hu.farcsal.cms.bean.PageBeanLocal;
+import hu.farcsal.cms.log.Log;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ *
+ * @author zoli
+ */
+class Beans {
+    
+    private static final Log LOGGER = Log.getLogger(Beans.class);
+    
+    public static PageBeanLocal lookupPageBeanLocal() {
+        try {
+            Context c = new InitialContext();
+            return (PageBeanLocal) c.lookup("java:global/hu.farcsal_CMS-ear_ear_0.1.1-SNAPSHOT/hu.farcsal_CMS-ejb_ejb_0.1.1-SNAPSHOT/PageBean!hu.farcsal.cms.bean.PageBeanLocal");
+        }
+        catch (NamingException ne) {
+            LOGGER.e("exception caught", ne);
+            return null;
+        }
+    }
+    
+}
---
+++ CMS-web/src/main/java/RewriteTestProvider.java
@@ -0,0 +1,86 @@
+import hu.farcsal.cms.util.UrlParameters;
+import javax.servlet.ServletContext;
+import org.ocpsoft.rewrite.annotation.RewriteConfiguration;
+import org.ocpsoft.rewrite.config.Condition;
+import org.ocpsoft.rewrite.config.Configuration;
+import org.ocpsoft.rewrite.config.ConfigurationBuilder;
+import org.ocpsoft.rewrite.config.Rule;
+import org.ocpsoft.rewrite.context.EvaluationContext;
+import org.ocpsoft.rewrite.event.Rewrite;
+import org.ocpsoft.rewrite.servlet.config.HttpConfigurationProvider;
+import org.ocpsoft.rewrite.servlet.config.rule.Join;
+import org.ocpsoft.rewrite.servlet.http.event.HttpOutboundServletRewrite;
+import org.ocpsoft.urlbuilder.AddressBuilder;
+
+/**
+ *
+ * @author zoli
+ */
+@RewriteConfiguration
+public class RewriteTestProvider extends HttpConfigurationProvider {
+    
+    private static class ParameterCondition implements Condition {
+
+        private final String value;
+        private final UrlParameters helper;
+        
+        public ParameterCondition(UrlParameters helper, String value) {
+            this.helper = helper;
+            this.value = value;
+        }
+        
+        @Override
+        public boolean evaluate(Rewrite rwrt, EvaluationContext ec) {
+            if (helper.getKey() != null && rwrt instanceof HttpOutboundServletRewrite) {
+                HttpOutboundServletRewrite event = (HttpOutboundServletRewrite) rwrt;
+                if (value == null) {
+                    removeParameter(event);
+                    return true;
+                }
+                String eventValue = helper.get(event.getAddress().toString());
+                if (eventValue == null) {
+                    removeParameter(event);
+                    return true;
+                }
+                boolean enabled = eventValue.equals(value);
+                if (enabled) {
+                    removeParameter(event);
+                }
+                return enabled;
+            }
+            return true;
+        }
+        
+        private void removeParameter(HttpOutboundServletRewrite event) {
+            event.setOutboundAddress(AddressBuilder.create(helper.remove(event.getAddress().toString())));
+        }
+        
+    }
+    
+    private static class LngCondition extends ParameterCondition {
+        
+        private static final UrlParameters helper = new UrlParameters("lang");
+        
+        public LngCondition(String value) {
+            super(helper, value);
+        }
+        
+    }
+    
+    @Override
+    public int priority() {
+        return 0;
+    }
+
+    @Override
+    public Configuration getConfiguration(final ServletContext context) {
+        Condition conditionHu = new LngCondition("hu");
+        Condition conditionEn = new LngCondition("en");
+        Rule ruleHu = Join.path("/tigris").to("/faces/tiger.xhtml");
+        Rule ruleEn = Join.path("/tiger").to("/faces/tiger.xhtml");
+        return ConfigurationBuilder.begin()
+            .addRule(ruleHu).when(conditionHu)
+            .addRule(ruleEn).when(conditionEn);
+    }
+    
+}
